d54d03ca6a0cc8287b7c5680c96b69629d72045f
Collo <mutaic84@gmail.com>
2022-03-12 13:14:50 +0300

    add onsubmit functionality to form alerting the user when they submit the form

--- js/main.js
+++ js/main.js
@@ -11,15 +11,23 @@ $(document).ready(function () {
         $(this).find(".overlay").slideUp(300)
     });
 
-    formSubmit()
-});
- 
-function formSubmit() { 
     $("form").submit(function (e) { 
         e.preventDefault();
         let name = $("input#name").val();
         let email = $("input#email").val();
         let message = $("textarea#message").val()
-        
+
+        if(name != "" && email != ""){
+            alert("Hello " + name + " @ " + email +" we have received your message. Thank you for reaching out to us. ");
+        } else {
+            alert("Please provide a name and valid email")
+        }
+        clearForm();
     });
- }
+    
+});
+    
+
+function clearForm() {
+    document.getElementById("contact-form").reset();
+}
